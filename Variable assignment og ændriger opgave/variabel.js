9127624a5c3c0018a4c48d81bca5446c82d9e5b6
Ahmady1997 <132337567+Qwesx131@users.noreply.github.com>
2023-08-28 18:06:10 +0200

    Add files via upload

---
+++ Variable assignment og ændriger opgave/variabel.js
@@ -0,0 +1,38 @@
+
+// Opgave 1 - Opret en variabel og tildel den en værdi:
+// Opret variabel og tildel værdi
+const Navn = "Ahmad Younis";
+
+// Udskriv værdien af variablen til konsollen
+console.log(Navn);
+
+
+//Opgave 2 - Beregning med variabler:
+
+const Tal1 = 6;
+const Tal2 = 5;
+const Sum = Tal1 + Tal2;
+
+console.log(Sum);
+
+
+//Opgave 3 - Ændre værdien af en variabel:
+let Alder = 26;
+Alder = 31;
+console.log(Alder);
+
+//Opgave 4 -  Increment og Decrement variabler
+
+let myNumber = 1;
+
+// Increment myNumber med 1
+myNumber++;
+console.log("Efter increment:", myNumber);
+
+// Decrement myNumber med 1
+myNumber--;
+console.log("Efter decrement:", myNumber);
+
+
+
+
